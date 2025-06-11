ac171751400c03f8331ffda7fd3312a2611bbd01
El Lizok <lizanechyporuk@gmail.com>
2025-06-11 13:36:47 +0300

    Deployment commit #2

--- src/backend/main.py
+++ src/backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://lizanechyporuk.github.io/nlp-project/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
